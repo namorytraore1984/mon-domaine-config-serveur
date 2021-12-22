5941b6f69270c1dbacc233aba595731e0f9e938b
Namory Traore <namorytraore1984@yahoo.fr>
2021-12-22 17:22:27 -0500

    Premier commit

---
+++ src/main/java/com/next/technologies/mondomaineconfigserveur/MonDomaineConfigServeurApplication.java
@@ -0,0 +1,13 @@
+package com.next.technologies.mondomaineconfigserveur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonDomaineConfigServeurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonDomaineConfigServeurApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/next/technologies/mondomaineconfigserveur/MonDomaineConfigServeurApplicationTests.java
@@ -0,0 +1,13 @@
+package com.next.technologies.mondomaineconfigserveur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonDomaineConfigServeurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
