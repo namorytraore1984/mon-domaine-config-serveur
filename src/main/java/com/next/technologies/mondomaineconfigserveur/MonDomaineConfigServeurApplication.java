06ee9373c7af15edafa25f5853a374ac4d72f54c
Namory Traore <namorytraore1984@yahoo.fr>
2022-01-02 00:02:51 -0500

    Integration avec git repo

--- src/main/java/com/next/technologies/mondomaineconfigserveur/MonDomaineConfigServeurApplication.java
+++ src/main/java/com/next/technologies/mondomaineconfigserveur/MonDomaineConfigServeurApplication.java
@@ -1,13 +1,20 @@
 package com.next.technologies.mondomaineconfigserveur;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 @SpringBootApplication
-public class MonDomaineConfigServeurApplication {
+@EnableConfigServer
+public class MonDomaineConfigServeurApplication implements CommandLineRunner {
 
 	public static void main(String[] args) {
 		SpringApplication.run(MonDomaineConfigServeurApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+	}
 }
